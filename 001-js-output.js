addf625e77ea2ceac60c32a865ced9e1c0004c4c
Jannat87 <fatemajannat.cse@gmail.com>
2026-08-27 20:52:49 +0600

    001-js-output-1 updated

--- 001-js-output.js
+++ 001-js-output.js
@@ -3,11 +3,41 @@
 console.log("Hello World");
 
 // 2. document.write()
+
+document.write("Hello Bina");
+
 // 3. alert()
+
 alert("missing the black belt");
+
 // 4. innerHTML *
+
+<p id="demo"></p>
+document.getElementById("demo").innerHTML = "Summer vacation";
+
 // 5. textContent *
+
+/*<p id="demo"></p>
+document.getElementById("demo").textContent = "Winter vacation";*/
+
 // 6. console.error()
+
+console.error("Somethingggg went wrong!!!!!!!");
+
 // 7. console.warn()
+
+console.warn("This is a  serious warning!!!!!!!!!");
+
 // 8. console.table()
+
+let student = {
+    name: "Jannat",
+    age: 22,
+    city: "Dhaka"
+};
+
+console.table(student);
+
 // 9. console.info()
+
+console.info("Website loaded successfully!1111111111111111");
