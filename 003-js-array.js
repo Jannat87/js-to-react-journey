0cc887d84b7a5e3c431f66fba026d0b902174818
Jannat87 <fatemajannat.cse@gmail.com>
2026-09-02 22:00:26 +0600

    js array

---
+++ 003-js-array.js
@@ -0,0 +1,8 @@
+var marks = new Array(40, 100, 1, 5, {size:25}, 10);
+console.log(marks);
+
+let fruits = ["Apple", "Mango", "Banana"];
+console.log(fruits[0]);
+
+fruits.push("Orange");
+console.log(fruits);
